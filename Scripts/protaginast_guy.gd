56822067b9635527750d82dbeb9f025fa2c5e685
rainsyy <fastaslightning101@gmail.com>
2023-10-29 13:15:17 -0400

    Added Enemy and Player Health  and Player Death
    Detetced when player got hit by enemies and decreases hp. Player dies at 0 hp

--- Scripts/protaginast_guy.gd
+++ Scripts/protaginast_guy.gd
@@ -2,43 +2,88 @@ extends CharacterBody2D
 
 @onready var animation = $AnimationPlayer
 
-var health = 100
+var HEALTH = 100
+var can_take_damage = true
+var player_alive = true
+var enemy_in_attack_zone = false
 const SPEED = 300.0
 
 func _physics_process(delta):
+	#every frame these things run
+	deal_damage()
+	player_move()
+
+	#if player health is 0 then kill player
+	if HEALTH <= 0:
+		player_alive = false 
+		HEALTH = 0
+		print("player killed")
+		self.queue_free()
+
+
+func player_move():
 	var yDirection = Input.get_axis("up","down")
+	var WizardSprite = $Wizard
 	if yDirection:
 		velocity.y = yDirection * SPEED
 	else:
 		velocity.y = move_toward(velocity.y,0,SPEED)
-	
+
 	var xDirection = Input.get_axis("left", "right")
+
 	if xDirection:
 		velocity.x = xDirection * SPEED
+		if xDirection > 0:
+			WizardSprite.flip_h = false
+		else:
+			WizardSprite.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
+
 	if yDirection or xDirection:
 		animation.play("Walk")
 	else:
 		animation.play("Idle")
-	
+
 	move_and_slide()
-	
-	
+
+
 func update_health():
 	var healthbar = $healthbar
-	
-	healthbar.value = health
-	
-	if health >= 100:
+
+	healthbar.value = HEALTH
+
+	if HEALTH >= 100:
 		healthbar.visible = false
 	else:
 		healthbar.visible = true
-		
+
 
 func _on_regin_timer_timeout():
-	if health < 100:
-		health = health + 5
-		if health > 100:
-			health = 100
+	if HEALTH < 100:
+		HEALTH = HEALTH + 5
+		if HEALTH > 100:
+			HEALTH = 100
+
+func _on_player_hitbox_area_entered(body):
+	print(body)
+	if body.get_parent().is_in_group("Enemy"):
+		enemy_in_attack_zone = true
+
+func _on_player_hitbox_area_exited(body):
+	if body.get_parent().is_in_group("Enemy"):
+		enemy_in_attack_zone = false
+
+func deal_damage():
+	if enemy_in_attack_zone == true:
+		if can_take_damage == true:
+			HEALTH = HEALTH - 20
+			$DamageTakeCooldown.start()
+			can_take_damage = false
+			print("Player Health is ", HEALTH)
+			if HEALTH <= 0:
+				self.queue_free()
+
+func _on_damage_take_cooldown_timeout():
+	can_take_damage = true
+
