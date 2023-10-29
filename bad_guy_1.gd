014ba058b5fa84bff5712e3b816c6f3eff6ebfd3
Luckac69 <Luckac69@gmail.com>
2023-10-28 22:00:03 -0400

    bad_guy1_fixed_sprite
    did the basics with BadGuy 1

--- bad_guy_1.gd
+++ bad_guy_1.gd
@@ -2,11 +2,14 @@ extends CharacterBody2D
 
 
 # var target = player
-
+@onready var animation = $AnimationPlayer
 
 const SPEED = 3
 
 func _physics_process(delta):
 	var player = get_parent().get_node("Protaginast_guy")
 	var target = player
+	
+	animation.play("Walk")
+	
 	position += (target.position - position).normalized() * SPEED
