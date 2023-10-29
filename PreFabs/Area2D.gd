d3bb7a3ed1ed2c449dd07d6dfa6ed3b58dfd6c7a
rainsyy <fastaslightning101@gmail.com>
2023-10-29 11:25:27 -0400

    Added main character and enemy hp bar
    Added animations to main chara and all

--- PreFabs/Area2D.gd
+++ PreFabs/Area2D.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
 func _on_area_entered(area):
-	print(area)
-	print("HasEntered")
+	print(area.name)
 
--- Scripts/protaginast_guy.gd
+++ Scripts/protaginast_guy.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
+@onready var animation = $AnimationPlayer
 
+var health = 100
 const SPEED = 300.0
 
 func _physics_process(delta):
@@ -16,6 +18,27 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
+	if yDirection or xDirection:
+		animation.play("Walk")
+	else:
+		animation.play("Idle")
+	
 	move_and_slide()
+	
+	
+func update_health():
+	var healthbar = $healthbar
+	
+	healthbar.value = health
+	
+	if health >= 100:
+		healthbar.visible = false
+	else:
+		healthbar.visible = true
+		
 
-
+func _on_regin_timer_timeout():
+	if health < 100:
+		health = health + 5
+		if health > 100:
+			health = 100
