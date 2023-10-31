c9abfe0ec4f33d6f5e81f9a09b9f59d4981e818b
Van <ninjasnivy101@gmail.com>
2023-10-31 17:34:43 -0400

    completed explosion
    spell bound to Q that launches a projectile that moves through enemies, dealing 50 damage every time it makes contact with an enemy. The projectile lasts for 4 seconds and this cooldown is refreshed each time an enemy is hit.

--- fireball.gd
+++ fireball.gd
@@ -4,7 +4,6 @@ extends CharacterBody2D
 @export var hit_effect : PackedScene
 
 var speed = 2000
-var velocitiy = Vector2(1, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
