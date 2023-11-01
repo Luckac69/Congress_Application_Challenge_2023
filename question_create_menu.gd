f0ff42767359690e9080a9e7af0ff770c7c16ca4
rainsyy <fastaslightning101@gmail.com>
2023-10-31 20:31:49 -0400

    Questions now save upon you creating them
    LETS GOOOOOOOOOOOOOOOOOOOOOOOO

--- question_create_menu.gd
+++ question_create_menu.gd
@@ -1,25 +1,34 @@
 extends Control
 
 @export var Question : TextEdit
-
 @export var Answer1 : TextEdit
 @export var Answer2 : TextEdit
 @export var Answer3 : TextEdit
 @export var Answer4 : TextEdit
+var amount : int = 0
 
+
+@onready var QuestionsFolder = get_node("Questions")
+@onready var questionsdictionary = $"/root/GlobalScript".QuestionDictionary
 
 
 func _on_create_button_button_up():
-	print(Question.text)
-	print(Answer1.text)
-	print(Answer2.text)
-	print(Answer3.text)
-	print(Answer4.text)
+	
+	var QuestionTable={
+		"Question":Question.text,
+		"Answer1":Answer1.text,
+		"Answer2":Answer2.text,
+		"Answer3":Answer3.text,
+		"Answer4":Answer4.text
+		}
+		
+	for Questions in questionsdictionary:
+		amount = amount + 1
+		
+	questionsdictionary["Question" + str(amount)] = QuestionTable
+	print(questionsdictionary)
 	
 	get_tree().change_scene_to_file("res://Scenes/study_menu.tscn")
-	
-	
-
 
 func _on_back_button_button_up():
 	get_tree().change_scene_to_file("res://Scenes/study_menu.tscn")
